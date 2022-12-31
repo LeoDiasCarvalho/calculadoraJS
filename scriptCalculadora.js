9d7b69852396e7d8e1ceb4c457cdebda0f7e5c37
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2022-12-31 14:28:51 -0300

    Implementado botao +/- e finalização do projeto

--- scriptCalculadora.js
+++ scriptCalculadora.js
@@ -1,101 +1,193 @@
-const visorAntesOperacao = document.querySelector("[acOperacao]");
-const visorDepoisOperacao = document.querySelector("[dcOperacao]");
-const btnLimpar = document.querySelector("[limpar]");
-const btnCorrigir = document.querySelector("[corrigir]");
-const btnOperacao = document.querySelectorAll("[operacao]");
 const btnNumeros = document.querySelectorAll("[numero]");
-const btnPorcento = document.querySelector("[porcentagem]");
+const btnOperacoes = document.querySelectorAll("[operacao]");
 const btnIgual = document.querySelector("[igual]");
+const btnCorrigir = document.querySelector("[corrigir]");
+const btnLimpar = document.querySelector("[limpar]");
+const visorCimaTexto = document.querySelector("[visorCima]");
+const visorBaixoTexto = document.querySelector("[visorBaixo]");
+const btnPorcento = document.querySelector("[porcentagem]");
+const btnMaisMenos = document.querySelector("[maisMenos]");
 
 class Calculadora {
-    constructor(antesOperacao, depoisOperacao){
-        this.visorAntesOperacao = antesOperacao;
-        this.visorDepoisOperacao = depoisOperacao;
-        this.limpar();
+  constructor(visorCimaTexto, visorBaixoTexto) {
+    this.visorCimaTexto = visorCimaTexto;
+    this.visorBaixoTexto = visorBaixoTexto;
+    this.limpar();
+  }
+
+  formatarNumeroDisplay(numero) {
+    const numeroTexto = numero.toString();
+
+    const digitoInteiro = parseFloat(numeroTexto.split(".")[0]);
+    const digitoDecimal = numeroTexto.split(".")[1];
+
+    let displayInteiro;
+
+    if (isNaN(digitoInteiro)) {
+      displayInteiro = "";
+    } else {
+      displayInteiro = digitoInteiro.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
     }
 
-    formatarNumeroDisplay(numero) {
-        const numeroTexto = numero.toString();
+    if (digitoDecimal != null) {
+      return `${displayInteiro}.${digitoDecimal}`;
+    } else {
+      return displayInteiro;
+    }
+  }
+
+  corrigir() {
+    this.visorBaixo = this.visorBaixo.toString().slice(0, -1);
+  }
+
+  calcularPorcentagem(){
+    let porcento;
+    let resultado;
     
-        const digitosInteiros = parseFloat(numeroTexto.split(".")[0]);
-        const digitosDecimais = numeroTexto.split(".")[1];
-    
-        let inteiroDisplay;
-    
-        if (isNaN(digitosInteiros)) {
-          inteiroDisplay = "";
-        } else {
-          inteiroDisplay = digitosInteiros.toLocaleString("en", {
-            maximumFractionDigits: 0,
-          });
-        }
-    
-        if (digitosDecimais != null) {
-          return `${inteiroDisplay}.${digitosDecimais}`;
-        } else {
-          return inteiroDisplay;
-        }
+    const _visorCima = parseFloat(this.visorCima);
+    const _visorBaixo = parseFloat(this.visorBaixo);
+
+    if(isNaN(_visorCima) || isNaN(_visorBaixo) || this.operacao == undefined) return;
+
+    porcento = _visorCima * (_visorBaixo / 100);
+
+    switch (this.operacao) {
+        case "+":
+          resultado = _visorCima + porcento;
+          break;
+        case "-":
+          resultado = _visorCima - porcento;
+          break;
+        case "/":
+          resultado = (_visorCima / _visorBaixo) * 100;
+          break;
+        case "x":
+          resultado = porcento;
+          break;
+        default:
+          return;
       }
 
-    escolhaDaOperacao(operacao){
-        if(this.dcOperacao === "") return;
+      this.visorBaixo = resultado;
+      this.visorCima = "";
+      this.operacao = undefined;
 
-        if(this.acOperacao !== ""){
-            calculadora.calcular();
-        }
+  }
 
-        this.operacao = operacao;
-        this.acOperacao = this.dcOperacao;
-        this.dcOperacao = "";
+  calcular() {
+    let resultado;
+    
+    const _visorCima = parseFloat(this.visorCima);
+    const _visorBaixo = parseFloat(this.visorBaixo);
 
+    if (isNaN(_visorCima) || isNaN(_visorBaixo) || this.operacao == undefined) return;
+
+    switch (this.operacao) {
+      case "+":
+        resultado = _visorCima + _visorBaixo;
+        break;
+      case "-":
+        resultado = _visorCima - _visorBaixo;
+        break;
+      case "/":
+        resultado = _visorCima / _visorBaixo;
+        break;
+      case "x":
+        resultado = _visorCima * _visorBaixo;
+        break;
+      default:
+        return;
     }
 
-    mostrarNumero(numero){
-        if(this.dcOperacao.includes(".") && numero === ".") return;
+    this.visorBaixo = resultado;
+    this.visorCima = "";
+    this.operacao = undefined;
+  }
 
-        this.dcOperacao = `${this.dcOperacao}${numero.toString()}`;
+  escolhaOperacao(operacao) {
+    if (this.visorBaixo === "") return;
+
+    if (this.visorCima !== "") {
+      this.calcular();
     }
 
-    limpar(){
-        this.acOperacao = "";
-        this.dcOperacao = "";
-        this.operacao = undefined;
-    }
+    this.operacao = operacao;
 
-    atualizarDisplay(){
-        this.visorAntesOperacao.innerText = `${this.formatarNumeroDisplay(this.acOperacao)} ${this.operacao || ""}`;
-        this.visorDepoisOperacao.innerText = this.formatarNumeroDisplay(this.dcOperacao);
-    }
+    this.visorCima = this.visorBaixo;
+    this.visorBaixo = "";
+  }
 
-    corrigir(){
-        this.dcOperacao = this.dcOperacao.toString().slice(0, -1);
-    }
+  mostrarNumero(numero) {
+    if (this.visorBaixo.includes(".") && numero === ".") return;
 
+    this.visorBaixo = `${this.visorBaixo}${numero.toString()}`;
+  }
+
+  limpar() {
+    this.visorBaixo = "";
+    this.visorCima = "";
+    this.operacao = undefined;
+  }
+
+  atualizarDisplay() {
+    this.visorCimaTexto.innerText = `${this.formatarNumeroDisplay(
+      this.visorCima
+    )} ${this.operacao || ""}`;
+    this.visorBaixoTexto.innerText = this.formatarNumeroDisplay(
+      this.visorBaixo
+    );
+  }
+
+  trocarSinal(){
+    this.visorBaixo = this.visorBaixo.charAt(0) === '-' 
+    ? this.visorBaixo.slice(1) 
+    : `-${this.visorBaixo}`
+
+  }     
 }
 
-const calculadora = new Calculadora(visorAntesOperacao, visorDepoisOperacao);
+const calculadora = new Calculadora(
+  visorCimaTexto,
+  visorBaixoTexto
+);
+
+for (const btnNumero of btnNumeros) {
+  btnNumero.addEventListener("click", () => {
+    calculadora.mostrarNumero(btnNumero.innerText);
+    calculadora.atualizarDisplay();
+  });
+}
+
+for (const btnOperacao of btnOperacoes) {
+  btnOperacao.addEventListener("click", () => {
+    calculadora.escolhaOperacao(btnOperacao.innerText);
+    calculadora.atualizarDisplay();
+  });
+}
 
 btnLimpar.addEventListener("click", () => {
-    calculadora.limpar();
-    calculadora.atualizarDisplay();
+  calculadora.limpar();
+  calculadora.atualizarDisplay();
+});
+
+btnIgual.addEventListener("click", () => {
+  calculadora.calcular();
+  calculadora.atualizarDisplay();
 });
 
 btnCorrigir.addEventListener("click", () => {
-    calculadora.corrigir();
+  calculadora.corrigir();
+  calculadora.atualizarDisplay();
+});
+
+btnPorcento.addEventListener("click", () => {
+    calculadora.calcularPorcentagem();
     calculadora.atualizarDisplay();
 });
 
-for(const numero of btnNumeros){
-    numero.addEventListener("click", () => {
-        calculadora.mostrarNumero(numero.innerText);
-        calculadora.atualizarDisplay();
-    });
-}
-
-for(const operacao of btnOperacao){
-    operacao.addEventListener("click", () => {
-        calculadora.escolhaDaOperacao(operacao.innerText);
-        calculadora.atualizarDisplay();
-    
-    })
-}
+btnMaisMenos.addEventListener("click", () => {
+  calculadora.trocarSinal();
+  calculadora.atualizarDisplay();
+});
